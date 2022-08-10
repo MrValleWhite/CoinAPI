ba938b6db592e1fee1a14357f654c0517bdf752d
MrValleWhite <58403779+MrValleWhite@users.noreply.github.com>
2022-08-10 22:34:10 +0200

    Changed lobbysystem

--- src/main/java/eu/raveq/coinapi/CoinSystem.java
+++ src/main/java/eu/raveq/coinapi/CoinSystem.java
@@ -15,6 +15,7 @@ public class CoinSystem extends JavaPlugin {
     @Override
     public void onLoad() {
         instance = this;
+        coinAPI = new CoinAPIImpl();
         this.mysql = MySQL.newBuilder()
                 .withUrl("135.125.194.35")
                 .withPort(3306)
